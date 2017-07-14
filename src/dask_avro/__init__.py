4e735ae0866c69d7009eb2b4de06d4b0f41ccf64
Rolando (Max) Espinoza <me@rmax.io>
2017-07-14 20:57:14 -0300

    Support multiple urlpaths.

--- src/dask_avro/__init__.py
+++ src/dask_avro/__init__.py
@@ -5,8 +5,10 @@ Disclaimer: The initial code was recovered from dask's distributed project.
 
 """
 import io
-import fastavro
+import itertools
 import json
+
+import fastavro
 
 from dask import delayed
 from dask.bytes import read_bytes
@@ -23,10 +25,12 @@ def read_avro(urlpath, blocksize=2**27, **kwargs):
 
     Parameters
     ----------
-    urlpath : string
+    urlpath : str or list[str]
         Absolute or relative filepath, URL or globstring pointing to avro files.
     blocksize : int, optional
-        Size of blocks. Default is 128MB.
+        Size of blocks. Note that this size must be larger than the files'
+        internal block size, otheriwse it will result in empty partitions.
+        Default is 128MB.
     **kwargs
         Additional arguments passed to ``dask.bytes.read_bytes`` function.
 
@@ -36,17 +40,36 @@ def read_avro(urlpath, blocksize=2**27, **kwargs):
         A list of delayed objects, one for each block.
 
     """
+    if isinstance(urlpath, str):
+        urlpath = [urlpath]
+    if not isinstance(urlpath, list):
+        raise TypeError("Invalid type for 'urlpath': %s" % type(urlpath))
+
+    return list(itertools.chain.from_iterable(
+        _read_avro(path, blocksize=blocksize, **kwargs) for path in urlpath
+    ))
+
+
+def _read_avro(urlpath, **kwargs):
+    """Read avro file in given path and returns a list of delayed objects."""
     myopen = OpenFileCreator(urlpath)
+
     values = []
     for fn in myopen.fs.glob(urlpath):
+
         with myopen(fn) as fp:
             av = fastavro.reader(fp)
             header = av._header
+
+        # TODO: If the avro block size in the file is larger than the blocksize
+        # passed here then some returned blocks may be empty because they don't
+        # contain the delimiter.
         _, blockss = read_bytes(fn, delimiter=header['sync'], not_zero=True,
-                                sample=False, blocksize=blocksize, **kwargs)
+                                sample=False, **kwargs)
         values.extend(
             delayed(_avro_body)(block, header) for blocks in blockss for block in blocks
         )
+
     if not values:
         raise ValueError("urlpath is empty: %s" % urlpath)
 
